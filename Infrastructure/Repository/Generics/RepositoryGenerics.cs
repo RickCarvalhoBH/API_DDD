3436e14ef653eceb5a9c6276bb6de47c6de8a11b
Milton Henrique Carvalho Pereira <milton.pereira@brtonline.com.br>
2023-05-19 08:14:52 -0300

    Adicionar arquivos de projeto.

---
+++ Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+    }
+}
