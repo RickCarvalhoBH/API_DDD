87ce37a217008c1375beaf8b02e47374c2fef388
Milton Henrique Carvalho Pereira <milton.pereira@brtonline.com.br>
2023-05-23 10:37:45 -0300

    Criado Token e outros.

---
+++ WebAPIs/Models/MessageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIs.Models
+{
+    public class MessageViewModel
+    {
+        [Column("MSN_ID")]
+        public int Id { get; set; }
+
+        [Column("MSN_TITULO")]
+        [MaxLength(255)]
+        public string Titulo { get; set; }
+
+        [Column("MSN_ATIVO")]
+        public bool Ativo { get; set; }
+
+        [Column("MSN_DATA_CADASTRO")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("MSN_DATA_ALTERACAO")]
+        public DateTime DataAlteracao { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+    }
+}
